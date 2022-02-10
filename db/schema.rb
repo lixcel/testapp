1a7064fa170c1d4d3861f5ecf719f435f49ff175
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-02-10 06:02:22 +0000

    [Finis]toppage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_044342) do
+ActiveRecord::Schema.define(version: 2022_02_10_055700) do
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
